d1234699dbb1fcaa300d8bc1042974d7e505e4cd
Ermich Reda <ermich.reda@ofppt-edu.ma>
2022-07-14 01:27:35 +0100

    add file .ignore

--- index.php
+++ index.php
@@ -1,14 +1,81 @@
+<?php
+require_once 'db/db.php';
+$error=null;
+try {
+    if(isset($pdo)){
+        $personnes=$pdo->query('SELECT * FROM personne')->fetchAll();
+    }
+}catch(PDOException $e){
+    $error=$e->getMessage();
+}
+?>
 <!--header-->
 <?php $root='./';require_once 'inc/header.php' ?>
-    <!--content-->
-    <div class="container" style="min-height: 75vh;">
-        <div class="row">
-            <div class="col-6 mx-auto">
-                <h1 class="display-1 my-5">Hello World!!!</h1>
-                <h1 class="display-1 my-5">Welcome Home</h1>
+
+<!--content-->
+<div class="container">
+    <?php if($error):?>
+        <div class="alert alert-danger">
+            <?=$error?>
+        </div>
+    <?php else: ?>
+        <h1 class="mt-4">Liste des Sportifs</h1>
+        <div class="card ">
+            <div class="card-header d-flex justify-content-between ">
+                <h2>Liste</h2>
+                <a href="#">
+                    <a href="ajout.php" role="button" class="btn btn-outline-success">Ajouter</a>
+                </a>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive-md">
+                    <table class="table table-bordered table-hover">
+                        <thead class="table-group-divider ">
+                        <tr class=" bg-info bg-opacity-25 text-opacity-100">
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Prenom</th>
+                            <th scope="col">Depart</th>
+                            <th scope="col">Email</th>
+                            <th scope="col" class="d-flex justify-content-center">action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($personnes as $personne):?>
+                            <tr>
+                                <th scope="row"><?=$personne->id?></th>
+                                <td><?=$personne->nom?></td>
+                                <td><?=$personne->prenom?></td>
+                                <td><?=$personne->depart?></td>
+                                <td><?=$personne->email?></td>
+                                <td class="d-flex justify-content-center">
+                                    <a href="crud/modifierC.php?id=<?=$personne->id?>" class="btn btn-outline-warning me-3">modifier</a>
+                                    <a href="crud/supprimer.php?id=<?=$personne->id?>" class="btn btn-outline-danger" onclick="return confirm('Delete this?');">supprimer</a>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="card-footer">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination  bg-gradient justify-content-center">
+                        <li class="page-item disabled">
+                            <a class="page-link">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
-    </div>
+    <?php endif ?>
+</div>
 
 <!--footer-->
 <?php require_once 'inc/footer.php' ?>
